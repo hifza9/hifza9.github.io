9a102b5222c0f2ea8e250daa43adedf999b1c3c2
hifza9 <hifzahameed6@gmail.com>
2023-01-20 18:57:54 -0500

    Inital commit for public repo

---
+++ script/app.js
@@ -0,0 +1,50 @@
+"use strict";
+
+(function(){
+
+
+    function DisplayHomePage() {
+        let AboutUsButton = document.getElementById("AboutUsBtn");
+        AboutUsButton.addEventListener("click", function (){
+            location.href = "about.html"
+        });
+    }
+    function  DisplayProductsPage(){
+
+    }
+
+    function DisplayServicesPage(){
+
+    }
+
+    function DisplayAboutUsPage(){
+
+    }
+    function DisplayContactPage(){
+
+    }
+
+    function Start() {
+        console.log("App Started!")
+        switch (document.title)
+        {
+            case "Home":
+                DisplayHomePage();
+                break;
+            case "Our Products":
+                DisplayProductsPage();
+                break;
+            case "Our Services":
+                DisplayServicesPage();
+                break;
+            case "About Us":
+                DisplayAboutUsPage();
+                break;
+            case "Contact Us":
+                DisplayContactPage();
+                break
+        }
+    }
+    window.addEventListener ("Load", Start)
+
+})()
