72bb58188d109b83787140a3ac02de816fdf93a5
hifza9 <hifzahameed6@gmail.com>
2023-01-20 19:54:56 -0500

    Make changes

--- script/app.js
+++ script/app.js
@@ -9,6 +9,30 @@
             location.href = "about.html"
         });
     }
+
+        let MainContent = document.getElementsByTagName("main");
+        let MainParagraph = document.createElement("p");
+
+        MainParagraph.setAttribute("id","MainParagraph");
+        MainParagraph.setAttribute("class","mt-3");
+        MainParagraph.textContent = "This is Main Paragraph"
+
+        MainContent.appendChild(MainParagraph);
+
+        let FirstString = "This is Main Paragraph";
+        let SecondString = '${FirstString} the Main Paragraph.' ;
+        MainParagraph.textContent = SecondString;
+
+        // CREATEARTICLE
+        let Article = document.createElement("article");
+        let ArticleParagraph = <p id="ArticleParagraph" class="mt-3"> This is my article</p>;
+        Article.setAttribute("class","container")
+        Article.innerHTML =ArticleParagraph;
+        DocumentBody.appendChild(Article);
+
+
+
+
     function  DisplayProductsPage(){
 
     }
